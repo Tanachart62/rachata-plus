70e11138c07b6a33ed3de543dd201f48f4a838eb
Tanachart62 <mick9883@gmail.com>
2026-09-09 17:51:56 +0700

    GOGOGO test api

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	router := gin.Default()
+
+	if err := router.SetTrustedProxies(nil); err != nil {
+		log.Fatal(err)
+	}
+
+	router.GET("/health", healthHandler)
+
+	if err := router.Run("127.0.0.1:8080"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"service": "rachata-plus-api",
+	})
 }
